491015f3de91445fcebc75f9ae521a433d5446e5
Raghav9525 <kshubham9525.gmail.com>
2023-11-04 23:49:29 +0530

    g

---
+++ src/Gallery.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Gallery() {
+  return (
+    <div>
+
+      {/* <!-- Gallery Section --> */}
+      <div class="containers gallery-section">
+        <div class="gallery-title">
+          <h1>Our Activities</h1>
+        </div>
+        <div class="gallery-images">
+          <img src={require(`./Gallery/9.jpg`)} alt="Image 1" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/1.jpg`)} alt="Image 3" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/2.jpg`)} alt="Image 2" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/10.jpg`)} alt="Image 4" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/3.jpg`)} alt="Image 5" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/5.jpg`)} alt="Image 6" class="gallery-image" title="[ACtivities Name]" />
+        </div>
+      </div>
+
+      <div class="containers gallery-section mt-2">
+        <div class="gallery-title">
+          <h1>ClassRoom Image</h1>
+        </div>
+        <div class="gallery-images">
+          <img src={require(`./Gallery/class2.jpg`)} alt="Image 1" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/class4.jpg`)} alt="Image 3" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/class1.jpg`)} alt="Image 2" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/class3.jpg`)} alt="Image 4" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/4.jpg`)} alt="Image 5" class="gallery-image" title="[ACtivities Name]" />
+          <img src={require(`./Gallery/9.jpg`)} alt="Image 6" class="gallery-image" title="[ACtivities Name]" />
+        </div>
+      </div>
+
+      <div className="containers gallery-section mt-2">
+      <div className="gallery-title">
+        <h1 style={{ textAlign: "center" }}>Videos</h1>
+      </div>
+      <div className="gallery-images">
+        <div className="video-row">
+          <video controls width="100%" height="auto">
+            <source src={require(`./Gallery/video1.mp4`)} type="video/mp4" />
+          </video>
+          <video controls width="100%" height="auto">
+            <source src={require(`./Gallery/video2.mp4`)} type="video/mp4" />
+          </video>
+        </div>
+        <div className="video-row">
+          <video controls width="100%" height="auto">
+            <source src={require(`./Gallery/video3.mp4`)} type="video/mp4" />
+          </video>
+          <video controls width="100%" height="auto">
+            <source src={require(`./Gallery/video4.mp4`)} type="video/mp4" />
+          </video>
+        </div>
+        <div className="video-row">
+          <video controls width="100%" height="auto">
+            <source src={require(`./Gallery/video5.mp4`)} type="video/mp4" />
+          </video>
+          <video controls width="100%" height="auto">
+            <source src={require(`./Gallery/video6.mp4`)} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Gallery
+
+
